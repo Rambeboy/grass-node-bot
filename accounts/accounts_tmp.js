5dd079d925f0011a21d5de2c895dac3c56844e46
Rambeboy <kingindoslayer@gmail.com>
2024-11-18 12:03:22 +0700

    first commit

---
+++ accounts/accounts_tmp.js
@@ -0,0 +1,16 @@
+/**
+ * Accounts list file
+ * write your accounts like this
+ * EMAILPASSWORD ACCOUNTYPE
+ * export const accounts = {
+ *     email: "YOUR EMAIL",
+ *     password: "YOUR PASSWORD",
+ * };
+ *
+ * USERID ACCOUNTTYPE
+ * export const accounts = "YOURUSERID"
+ */
+export const accounts = {
+  email: "YOUR EMAIL",
+  password: "YOUR PASSWORD",
+};
